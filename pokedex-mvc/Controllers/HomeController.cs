0788db75f768e6e40af0efe4b8de6d4d1c445700
Tomas Avalos <tomasgonzaloavalos@gmail.com>
2025-09-24 19:49:19 -0300

    primer commit

---
+++ pokedex-mvc/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using pokedex_mvc.Models;
+using System.Diagnostics;
+
+namespace pokedex_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult ListaPokemon()
+        {
+            return View("ListaPokemon");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
